a1f99eb98b3744f156d723b66c96cb409031c4d7
wolf-jpg <66335345+wolf-jpg@users.noreply.github.com>
2020-10-15 17:30:37 -0500

    Add files via upload

---
+++ phyton.py
@@ -0,0 +1,18 @@
+i = ("Programa hecho ")
+x = ("por mi")
+resultado = i + x
+print(resultado)
+numeroUno = (20)
+numeroDos = (10)
+suma = numeroUno + numeroDos
+resta = numeroUno - numeroDos
+div = numeroUno * numeroDos
+sonIgualesONo = numeroUno == numeroDos
+print ("el resultado de la suma es")   
+print(suma)
+print ("el resultado de la resta es")   
+print(numeroUno - numeroDos)
+print ("el resultado de la division es")   
+print(numeroUno * numeroDos)
+print ("el resultado de la comparacion es")   
+print(numeroUno == numeroDos)
